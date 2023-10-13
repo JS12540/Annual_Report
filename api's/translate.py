ae3102d43ee0abca315cecbc15705229a1a3b43a
JS12540 <jayshah0726@gmail.com>
2023-10-13 12:00:46 +0530

    Added voice assistant api

--- api's/translate.py
+++ api's/translate.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
+from gtts import gTTS
+import io
 import translate
 
 app = FastAPI()
@@ -24,17 +26,40 @@ def translate_message(language: str, message: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+def voice_assistant(language: str, message: str):
+    try:
+        tts = gTTS(message, lang=language)
+        audio_stream = io.BytesIO()
+        tts.save(audio_stream)
+        audio_stream.seek(0)
+        return audio_stream
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 @app.post("/translate")
-async def translate(request: Request):
+async def translate_endpoint(request: Request):
     data = await request.json()
     language = data.get("language")
     message = data.get("message")
-    
+
     if not language or not message:
         raise HTTPException(status_code=400, detail="Language and message are required fields.")
-    
+
     translated_message = translate_message(language, message)
     return {"translated_message": translated_message}
+
+@app.post("/voice_assistant")
+async def voice_assistant_endpoint(request: Request):
+    data = await request.json()
+    language = data.get("language")
+    message = data.get("message")
+
+    if not language or not message:
+        raise HTTPException(status_code=400, detail="Language and message are required fields.")
+
+    audio_stream = voice_assistant(language, message)
+    return Response(content=audio_stream, media_type="audio/mpeg")
 
 if __name__ == "__main__":
     import uvicorn
